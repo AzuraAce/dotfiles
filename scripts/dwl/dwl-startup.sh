29da36d97dbb82eb2b08ae56c7c87dc06bfa178a
AzuraAce <oliverjbmub@gmail.com>
2025-09-19 01:09:06 +0200

    start mpd

--- scripts/dwl/dwl-startup.sh
+++ scripts/dwl/dwl-startup.sh
@@ -7,5 +7,6 @@ swaybg --output '*' --mode fill --image ~/wallpapers/pokemon_windows_7.jpg &
 mako &
 foot --server &
 swayidle -w lock 'swaylock' &
+mpd &
 
 exec dbus-update-environment WAYLAND_DISPLAY
